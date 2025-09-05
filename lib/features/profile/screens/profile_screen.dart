8d62dc561c6eb37929f2dcc44d60773d68175edd
Yerkin Zhaksylykov <yerkinzhaksylykov@gmail.com>
2025-09-05 15:11:38 +0500

    feat-profile_items

--- lib/features/profile/screens/profile_screen.dart
+++ lib/features/profile/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:qmed_employee/core/const/color_styles.dart';
 import 'package:qmed_employee/core/get_it/injection_container.dart';
 import 'package:qmed_employee/features/profile/logic/bloc/profile_bloc.dart';
@@ -12,6 +13,7 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  bool isOn = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +24,230 @@ class _ProfileScreenState extends State<ProfileScreen> {
           child: BlocBuilder<ProfileBloc, ProfileState>(
             builder: (context, state) {
               if(state is ProfileLoading){
-                return const CircularProgressIndicator();
+                return Center(child: const CircularProgressIndicator());
               }
               if(state is ProfileSuccess){
                 return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    
+                    Column(
+                      children: [
+                        Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                      height: 140,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 7,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 20,top: 30),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text('${state.response.lastName} ${state.response.firstName}',
+                                      style: GoogleFonts.montserrat(fontSize: 13,fontWeight: FontWeight.w500,color: ColorStyles.whiteColor),),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text('${state.response.middleName}',
+                                      style: GoogleFonts.montserrat(fontSize: 13,fontWeight: FontWeight.w500,color: ColorStyles.whiteColor),),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text('${state.response.mail}',
+                                      style: GoogleFonts.montserrat(fontSize: 13,fontWeight: FontWeight.w500,color: ColorStyles.whiteColor),),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 5,
+                            child: Image.asset('assets/images/png/logo.png',width: 140,height: 120,)
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: ColorStyles.whiteColor,
+
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Личные данные',
+                                    style: GoogleFonts.montserrat(fontSize: 13,color: ColorStyles.blackColor,fontWeight: FontWeight.w500),),
+                                     Text('Просмотреть личные данные',
+                                    style: GoogleFonts.montserrat(fontSize: 11,color: ColorStyles.blackColor,fontWeight: FontWeight.w400),),
+                                  ],
+                                ),
+                                Icon(Icons.arrow_forward_ios_outlined,color: ColorStyles.primaryColor,size: 16,)
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: ColorStyles.whiteColor,
+
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Политика конфиденциальности',
+                                    style: GoogleFonts.montserrat(fontSize: 13,color: ColorStyles.blackColor,fontWeight: FontWeight.w500),),
+                                     Text('Нажмите для ознакомления',
+                                    style: GoogleFonts.montserrat(fontSize: 11,color: ColorStyles.blackColor,fontWeight: FontWeight.w400),),
+                                  ],
+                                ),
+                                Icon(Icons.arrow_forward_ios_outlined,color: ColorStyles.primaryColor,size: 16,)
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: ColorStyles.whiteColor,
+
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Смена языка',
+                                    style: GoogleFonts.montserrat(fontSize: 13,color: ColorStyles.blackColor,fontWeight: FontWeight.w500),),
+                                     Text('Русский язык',
+                                    style: GoogleFonts.montserrat(fontSize: 11,color: ColorStyles.blackColor,fontWeight: FontWeight.w400),),
+                                  ],
+                                ),
+                                Icon(Icons.arrow_forward_ios_outlined,color: ColorStyles.primaryColor,size: 16,)
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: ColorStyles.whiteColor,
+
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Смена темы',
+                                    style: GoogleFonts.montserrat(fontSize: 13,color: ColorStyles.blackColor,fontWeight: FontWeight.w500),),
+                                     Text('Светлая',
+                                    style: GoogleFonts.montserrat(fontSize: 11,color: ColorStyles.blackColor,fontWeight: FontWeight.w400),),
+                                  ],
+                                ),
+                                Icon(Icons.arrow_forward_ios_outlined,color: ColorStyles.primaryColor,size: 16,)
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: ColorStyles.whiteColor,
+
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Уведомления',
+                                    style: GoogleFonts.montserrat(fontSize: 13,color: ColorStyles.blackColor,fontWeight: FontWeight.w500),),
+                                     Text('Настройте уведомления приложения',
+                                    style: GoogleFonts.montserrat(fontSize: 11,color: ColorStyles.blackColor,fontWeight: FontWeight.w400),),
+                                  ],
+                                ),
+                                Switch(
+                                  value: isOn,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      isOn = value;
+                                    });
+                                  },
+                                  activeColor: Colors.white,                 // цвет кружка (вкл)
+                                  activeTrackColor: const Color(0xFFA3763F), // коричневый фон
+                                  inactiveThumbColor: Colors.white,          // цвет кружка (выкл)
+                                  inactiveTrackColor: Colors.grey.shade300,  // серый фон
+                                ),
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                      ],
+                    ),
+
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 20),
+                      child: Container(
+                       padding: const EdgeInsets.all(12),
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(12),
+                        color: ColorStyles.whiteColor,
+                        border: Border.all(color: Colors.red,width: 1)
+                       ),
+                       child: Row(
+                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                         children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                             children: [
+                               Text('Выйти из аккаунта',
+                               style: GoogleFonts.montserrat(fontSize: 13,color: Colors.red,fontWeight: FontWeight.w500),),
+                                Text('Нажмите, чтобы выйти',
+                               style: GoogleFonts.montserrat(fontSize: 11,color: Colors.red,fontWeight: FontWeight.w400),),
+                             ],
+                           ),
+                           Icon(Icons.arrow_forward_ios_outlined,color: Colors.red,size: 16,)
+                         ],
+                       ),
+                                        ),
+                    ),
+
                   ]
                 );
               }
